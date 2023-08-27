9c403241a85b197ecead6fbc5a87120c959a6f9d
Michael-Obj <michaeloluwole99@gmail.com>
2023-08-27 20:12:32 +0100

    i changed the path in the urls.py

--- bosstwo/urls.py
+++ bosstwo/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
    path("stafftwos", views.RegisterStaff),
    path("staff/<str:staffId>/", views.GetById),
    path("stafflevel/<str:levelId>/", views.GetByStaffId),
-   # path("staff/edit/<str:id>/", views.UpdateStaff),
-   # path("staff/delete/<str:id>/", views.DeleteStaff)
+   path("staff/edit/<str:id>/", views.UpdateStaff),
+   path("staff/delete/<str:id>/", views.DeleteStaff)
 ]
